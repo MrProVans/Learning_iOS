eb1e9349d6e5acdb15d4067b629b31a0d6b99ca5
MrProVans <77919630+MrProVans@users.noreply.github.com>
2026-02-05 13:37:17 +0300

    2

--- Learning_iOS/ContentView.swift
+++ Learning_iOS/ContentView.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var counter = 0
+    @State private var isPressed = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            Text("Привет, iOS 👋")
+                .font(.largeTitle)
+                .foregroundStyle(.blue)
+            
+            Text("Нажатий: \(counter)")
+                .font(.title2)
+            
+            Button {
+                withAnimation {
+                    counter += 1
+                } } label: {
+                    Text("Нажми меня")
+                        .font(.headline)
+                        .padding(.horizontal, 24)
+                        .padding(.vertical, 12)
+                        .background(Color.blue)
+                        .foregroundStyle(.white)
+                        .cornerRadius(12)
+                        .scaleEffect(isPressed ? 0.95 : 1.0)
+                }
+                .buttonStyle(.plain)
+                            .onLongPressGesture(minimumDuration: 0.01, pressing: { pressing in
+                                withAnimation(.easeInOut(duration: 0.1)) {
+                                    isPressed = pressing
+                                }
+                            }, perform: {})
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
     }
 }
---
+++ Learning_iOS/workplace.swift
@@ -0,0 +1,7 @@
+//
+//  workplace.swift
+//  Learning_iOS
+//
+//  Created by Иван Клочков on 05.02.2026.
+//
+

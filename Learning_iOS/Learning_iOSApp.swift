fcab65dc7e11ca0935d48bb464e075a1c359b95d
MrProVans <77919630+MrProVans@users.noreply.github.com>
2026-02-05 12:17:38 +0300

    Initial Commit

---
+++ Learning_iOS/Learning_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_iOSApp.swift
+//  Learning_iOS
+//
+//  Created by Иван Клочков on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Learning_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
